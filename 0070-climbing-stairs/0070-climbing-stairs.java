160cdbd09aad6217f247c5996cad0f946c98826f
Mirjaj Ajij Milon <mirjajajijmilon@gmail.com>
2026-08-29 17:39:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,24 @@
 class Solution {
-    private static int helper(int n, int dp[]){
-        if(n == 0 ) return 1;
-        if(n < 0) return 0;
+    // private static int helper(int n, int dp[]){
+    //     if(n == 0 ) return 1;
+    //     if(n < 0) return 0;
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = helper(n-1, dp) + helper(n-2, dp);  
-        return dp[n];
-    }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     dp[n] = helper(n-1, dp) + helper(n-2, dp);  
+    //     return dp[n];
+    // }
     public int climbStairs(int n) {
         int dp[] = new int[n + 1];
-        Arrays.fill(dp, -1);
-
-        return helper(n, dp);
+        dp[0] = 1;
+        for(int i=1; i<=n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1] + 0;
+            }else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
     }
 }
